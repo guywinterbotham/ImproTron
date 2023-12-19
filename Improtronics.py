4cce5a9adaa3abe4332746ea69ffe49fecc87e02
Guy Winterbotham <guy.winterbotham@gmail.com>
2023-12-19 14:56:31 -0500

    New UI files for ImproTron and its control panel. Python for new UIs

---
+++ Improtronics.py
@@ -0,0 +1,240 @@
+# The display is a container for all the possible features that can be displayed
+# This Python file uses the following encoding: utf-8
+import json
+from PySide6.QtWidgets import QFileDialog, QPushButton, QMainWindow, QLineEdit, QLabel, QVBoxLayout, QWidget
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QDir, QStandardPaths, Slot
+from PySide6.QtGui import QPixmap, QImage, QImageReader, QFont
+from screeninfo import get_monitors
+
+# Class to handle display on a separate monitor
+class ImproTron(QMainWindow):
+    def __init__(self, parent=None):
+        super(ImproTron, self).__init__()
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle("Display 1")
+        self.loader = QUiLoader()
+        self.improTron = self.loader.load("Improtron.ui", None)
+        self.setCentralWidget(self.improTron)
+        # Store some default text formating
+        self.labelCSS = self.improTron.textDisplay.styleSheet()
+        self.text_font = self.improTron.textDisplay.font()
+        self.maximize()
+
+    # Colorize the left score display
+    def colorizeLeftScore(self, labelCSS):
+        self.improTron.leftTeamLabel.setStyleSheet(labelCSS)
+        self.improTron.leftScoreLCD.setStyleSheet(labelCSS)
+
+    # Colorize the right score display
+    def colorizeRightScore(self, labelCSS):
+        self.improTron.rightTeamLabel.setStyleSheet(labelCSS)
+        self.improTron.rightScoreLCD.setStyleSheet(labelCSS)
+
+    # Colorize the right score display
+    def colorizeTextDisplay(self, labelCSS):
+        self.labelCSS = labelCSS
+        self.improTron.textDisplay.setStyleSheet(self.labelCSS)
+
+    # Clear the display to black
+    def blackout(self):
+        self.improTron.textDisplay.setStyleSheet("background:black; color:black")
+        self.improTron.textDisplay.setText("blackout")
+        self.improTron.setCurrentWidget(self.improTron.displayText)
+
+    # Change the font size for the text
+    @Slot(QFont)
+    def sizeTextFont(self, new_font):
+        size = self.text_font.pointSize()
+        self.text_font = new_font
+        self.text_font.setPointSize(size)
+        self.improTron.textDisplay.setFont(self.text_font)
+
+    # Change the font size for the text
+    @Slot(int)
+    def sizeTextDisplay(self, size):
+        if size > 0:
+            self.text_font.setPointSize(size)
+            self.improTron.textDisplay.setFont(self.text_font)
+
+    # Show Text on the disaply
+    @Slot(str,str,int)
+    def showText(self, text_msg):
+        self.improTron.textDisplay.setText(text_msg)
+        self.improTron.textDisplay.setStyleSheet(self.labelCSS)
+        self.improTron.setCurrentWidget(self.improTron.displayText)
+
+    # Show Text on the disaply
+    @Slot(QImage)
+    def showImage(self, arg):
+        self.improTron.textDisplay.setPixmap(QPixmap.fromImage(arg.scaled(self.improTron.textDisplay.size())))
+        self.improTron.setCurrentWidget(self.improTron.displayText)
+
+    # Set the name of the Right Team
+    @Slot(str)
+    def showRightTeam(self, teamName):
+        self.improTron.rightTeamLabel.setText(teamName)
+
+    # Set the name of the Left Team
+    @Slot(str)
+    def showLeftTeam(self, teamName):
+        self.improTron.leftTeamLabel.setText(teamName)
+
+    # Update the scores on the score board
+    @Slot(int, int)
+    def updateScores(self, argLeft, argRight):
+        self.improTron.leftScoreLCD.setText(str(argLeft))
+        self.improTron.rightScoreLCD.setText(str(argRight))
+        self.improTron.setCurrentWidget(self.improTron.displayScore)
+
+    @Slot()
+    def maximize(self):
+        self.showFullScreen()
+
+    @Slot()
+    def restore(self):
+        self.showNormal()
+# End Class ImproTron
+
+
+class HotButton():
+    def __init__(self, button_number, controlBoard, improtron):
+
+        self.button_number = button_number
+        self.text = "Button "+str(button_number)
+        self.control_board = controlBoard
+        self.display = improtron
+        # Take control of the actual button
+        self.hot_button = controlBoard.improtronControlPanel.findChild(QPushButton, "hotPB" +str(button_number))
+        self.hot_button.clicked.connect(self.hotButtonClicked)
+
+        self.hot_button_title = controlBoard.improtronControlPanel.findChild(QLineEdit, "titleHotButton" +str(button_number))
+        self.hot_button_title.textChanged.connect(self.hotButtonNameChange)
+        self.hot_button_title.setText(self.text)
+
+        self.hot_button_image_file = controlBoard.improtronControlPanel.findChild(QLineEdit, "imageFileTxt" +str(button_number))
+        self.hot_button_image_file.setText("C:\\Users\\guywi\\OneDrive\\Pictures\\Roanoke\\PICT0339.JPG")
+
+        self.hot_button_select_file = controlBoard.improtronControlPanel.findChild(QPushButton, "selectPB" +str(button_number))
+        self.hot_button_select_file.clicked.connect(self.selectImage)
+
+    def clear(self):
+        self.hot_button_image_file.clear()
+        self.hot_button_title.clear()
+        self.text = "Button "+str(self.button_number)
+        self.hot_button.setText(self.text)
+
+    def save(self, hot_buttons_json):
+        hot_buttons_json[self.hot_button_title.objectName()] = self.hot_button_title.text()
+        hot_buttons_json[self.hot_button_image_file.objectName()] = self.hot_button_image_file.text()
+
+    def load(self, hot_buttons_json):
+        self.hot_button_title.setText(hot_buttons_json[self.hot_button_title.objectName()])
+        self.hot_button_image_file.setText(hot_buttons_json[self.hot_button_image_file.objectName()])
+
+    @Slot(str)
+    def hotButtonNameChange(self,new_name):
+        self.hot_button.setText(new_name)
+        self.text = new_name
+
+    @Slot()
+    def selectImage(self):
+        dialog = QFileDialog(self.control_board)
+        locations = QStandardPaths.standardLocations(QStandardPaths.PicturesLocation)
+        directory = locations[-1] if locations else QDir.currentPath()
+        dialog.setDirectory(directory)
+        fileName = QFileDialog.getOpenFileName(self.control_board, "Open Image", "" , "Image Files (*.png *.jpg *.bmp)")
+        self.hot_button_image_file.setText(fileName[0])
+
+    @Slot()
+    def hotButtonClicked(self):
+        reader = QImageReader(self.hot_button_image_file.text())
+        reader.setAutoTransform(True)
+        newImage = reader.read()
+
+        # Scale to match the preview
+        self.control_board.imagePreview.setPixmap(QPixmap.fromImage(newImage.scaled(self.control_board.imagePreview.size())))
+        self.display.showImage(newImage)
+
+#Code for returning screen information
+
+class MonitorInfoApp(QWidget):
+    def __init__(self):
+        super(MonitorInfoApp, self).__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QVBoxLayout()
+
+        # Get a list of connected monitors
+        monitors = get_monitors()
+
+        if not monitors:
+            layout.addWidget(QLabel("No monitors found"))
+        else:
+            for i, monitor in enumerate(monitors, 1):
+                monitor_label = QLabel(f"Monitor {i}: {monitor.name}, {monitor.width}x{monitor.height} pixels, Primary{monitor.is_primary}")
+                layout.addWidget(monitor_label)
+
+        self.setLayout(layout)
+        self.setWindowTitle("Monitor Information")
+        self.show()
+
+class HotButtonManager():
+    # Instantiate the Hot Buttons via a Hot Button Object that takes care of the signal and slots
+    # leveraging the naming convention
+    def __init__(self, controlBoard, improtron):
+        self.hot_buttons = [] #empty array
+        self.number = 10      #number of hotbuttons
+        self.file_name = "C:\\Users\\guywi\\AppData\\Local\\ImproTron\\improtron_hotbuttons.json"
+        self.control_board = controlBoard
+
+        for button in range(self.number):
+            self.hot_buttons.append(HotButton(button+1, controlBoard, improtron))
+
+        # Set a slot for the clear, load and save buttons
+        controlBoard.hotButtonClearPB.clicked.connect(self.clearHotButtonsClicked)
+        controlBoard.hotButtonLoadPB.clicked.connect(self.loadHotButtonsClicked)
+        controlBoard.hotButtonSavePB.clicked.connect(self.saveHotButtonsClicked)
+
+    @Slot()
+    def clearHotButtonsClicked(self):
+        for button in range(self.number):
+            self.hot_buttons[button].clear()
+
+    @Slot()
+    def loadHotButtonsClicked(self):
+        dialog = QFileDialog(self.control_board)
+        dialog.setDirectory('C:/Users/guywi/AppData/Local/ImproTron')
+        fileName = QFileDialog.getOpenFileName(self.control_board, "Load Hot Buttons",
+                    "C:/Users/guywi/AppData/Local/ImproTron/improtron_hotbuttons.json" ,
+                    "Config Files(*.json)")
+
+        # Read the JSON data from the file
+        with open(fileName[0], 'r') as json_file:
+            button_data = json.load(json_file)
+
+        for button in range(self.number):
+            self.hot_buttons[button].load(button_data)
+
+    @Slot()
+    def saveHotButtonsClicked(self):
+        dialog = QFileDialog(self.control_board)
+        dialog.setDirectory('C:/Users/guywi/AppData/Local/ImproTron')
+        fileName = QFileDialog.getSaveFileName(self.control_board, "Save Hot Buttons",
+                                   "C:/Users/guywi/AppData/Local/ImproTron/improtron_hotbuttons.json",
+                                   "Config Files (*.json)")
+        button_data = {}
+        for button in range(self.number):
+            self.hot_buttons[button].save(button_data)
+
+        # Convert the Python dictionary to a JSON string
+        json_data = json.dumps(button_data, indent=2)
+
+        print('file is', fileName)
+
+        # Write the JSON string to a file
+        with open(fileName[0], 'w') as json_file:
+            json_file.write(json_data)
+
