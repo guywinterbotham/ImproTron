0871ed002c3cd31d3a6611168fe5418c74fdbc60
Guy Winterbotham <guy.winterbotham@gmail.com>
2025-11-22 20:44:59 -0500

    Updated user guide and build batch file

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-@echo off
+@echo on
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 cd /d "C:\Users\guywi\OneDrive\Documents\git\ImproTron"
 .qtcreator\Python_3_12_10venv\Scripts\activate
-qtcreator\Python_3_12_10venv\Scripts\pyside6-deploy -c ImproTron.spec
+.qtcreator\Python_3_12_10venv\Scripts\pyside6-deploy -c ImproTron.spec
